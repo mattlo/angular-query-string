8a7d64bd7aabc28638e838285e9358914b5125fd
Matt Lo <mattlo.developer@gmail.com>
2014-06-27 17:52:40 -0500

    added query string impl

--- angular-query-string.js
+++ angular-query-string.js
@@ -1,3 +1,25 @@
 (function () {
-	angular.module('angular-query-string', ['ng']);
+	angular.module('angular-query-string', ['ng']).service("UrlQueryString", ['$window', function ($window) {
+		var output = {},
+			i,
+			pairFragments,
+			keyValues;
+		
+		if (window.location.search.length > 0) {
+			pairFragments = window.location.search.substr(1).split('&');
+			
+			for (i = 0; i < pairFragments.length; ++i) {
+				// has key and value
+				if (pairFragments[i].indexOf('=') > -1) {
+					keyValues = pairFragments[i].split('=');
+					
+					output[keyValues[0]] = keyValues[1];
+				} else { // only key
+					output[pairFragments[i]] = null;
+				}
+			}
+		}
+		
+		return output;
+	}]);
 }());
